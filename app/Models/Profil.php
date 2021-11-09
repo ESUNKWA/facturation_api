ac62a58102f01f428c90e943adc26fc635b5d8ed
ESUNKWA <kouadiodeki@gmail.com>
2021-11-09 10:54:52 +0000

    Profil

--- app/Models/Profil.php
+++ app/Models/Profil.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Profil extends Model
 {
     use HasFactory;
-    protected $table = 't_profil';
+    protected $table = 't_profils';
     protected $primaryKey = 'r_i';
     protected $fillable = ['r_libelle', 'r_description', 'r_status'];
 }
